fd4d0aa04dbd9c8907454e80474f1a7713ab08bf
Leif Battermann <leif.battermann@gmail.com>
2018-02-06 19:53:15 +0100

    added comments, updated reademe, added tests

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # A parser combinator hands on
 
-based on "Programming in Haskell" by Graham Hutton.
+based on [Programming in Haskell](http://www.cs.nott.ac.uk/~pszgmh/pih.html) by Graham Hutton.
+
+## Part 2 - Sequencing and choice
+
+Implement all incomplete functions marked with `???` and make sure all tests pass.
+
+If all tests pass proceed to part 3:
+
+    git checkout part3
+    
+(you have to commit or reset your changes)
---
+++ src/test/scala/parsers/ParsersTests.scala
@@ -0,0 +1,61 @@
+package parsers
+
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen
+import org.scalatest._
+import org.scalatest.prop._
+
+class ParsersTests extends PropSpec with PropertyChecks with Matchers {
+
+  private val nonEmptyString =
+    Gen.nonEmptyListOf(arbitrary[Char]).map(_.mkString)
+
+  property("succeed should always succeed with the result value v") {
+    forAll(nonEmptyString, arbitrary[Int]) { (input, v) =>
+      parsers.succeed(v)(input) shouldEqual Some((v, input))
+    }
+  }
+
+  property("fail should always fail") {
+    forAll(arbitrary[String]) { input =>
+      parsers.fail(input) shouldEqual None
+    }
+  }
+
+  property("item should succeed with first char for non empty inputs") {
+    forAll(nonEmptyString) { input =>
+      item(input) shouldEqual Some((input.charAt(0), input.substring(1)))
+    }
+  }
+
+  property("item should fail if input is empty") {
+    item("") shouldEqual None
+  }
+
+  property("sequencing succeed") {
+    val p = for {
+      v1 <- item
+      _ <- item
+      v2 <- item
+    } yield s"$v1$v2"
+
+    "abc".parse(p) shouldEqual Some(("ac", ""))
+  }
+
+  property("sequencing fail") {
+    val p = for {
+      v1 <- item
+      _ <- item
+      v2 <- item
+    } yield s"$v1$v2"
+
+    "ab".parse(p) shouldEqual None
+  }
+
+  property("choice") {
+    forAll(arbitrary[String]) { input =>
+      val p = parsers.fail +++ parsers.succeed(42)
+      input.parse(p) shouldEqual Some((42, input))
+    }
+  }
+}
