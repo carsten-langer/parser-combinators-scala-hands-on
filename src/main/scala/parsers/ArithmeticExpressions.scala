e05450820bd0eb0cf9e5a47a26c0f1341aa22b72
Leif Battermann <leif.battermann@gmail.com>
2018-02-21 19:37:12 +0100

    docs

--- src/main/scala/parsers/ArithmeticExpressions.scala
+++ src/main/scala/parsers/ArithmeticExpressions.scala
@@ -11,12 +11,44 @@ object ArithmeticExpressions {
    nat ::= 0 | 1 | 2 | \dots \
    */
 
+  /** Parses an expression according to:
+    *
+    * expr ::= term (+ expr | \epsilon)
+    *
+    * Ignores spaces around tokens.
+    *
+    * Example:
+    *
+    * {{{
+    *   expr("2*(3+4)") == Some((14, ""))
+    * }}}
+    *
+    */
   lazy val expr: Parser[Int] = ???
 
+  /** Parses terms according to:
+    *
+    * term ::= factor (* term | \epsilon)
+    *
+    */
   lazy val term: Parser[Int] = ???
 
+  /** Parses factors according to:
+    *
+    * factor ::= (expr) | nat
+    * nat ::= 0 | 1 | 2 | ...
+    *
+    */
   lazy val factor: Parser[Int] = ???
 
+  /** Evaluates an input string to its integer representation.
+    *
+    * Returns an `Int` if the string can be parsed and evaluated successfully without any remaining unconsumed string.
+    * Otherwise it returns an error message.
+    *
+    * @param input input string
+    * @return a result that is either a `String` or an `Int`
+    */
   def eval(input: String): Either[String, Int] =
     expr(input) match {
       case Some((n, ""))  => Right(n)
--- src/main/scala/parsers/ListParsers.scala
+++ src/main/scala/parsers/ListParsers.scala
@@ -1,6 +1,16 @@
 package parsers
 
 object ListParsers {
+
+  /** A parser for lists of natural numbers that ignores spaces around tokens.
+    *
+    * {{{
+    *   nonEmptyIntList("[1, 2, 3, 4] abc") == Some((List(1, 2, 3, 4), "abc"))
+    *
+    *   nonEmptyIntList("[1, 2,] abc") == None
+    * }}}
+    *
+    */
   val nonEmptyIntList: Parser[List[Int]] =
     for {
       _ <- symbol("[")
